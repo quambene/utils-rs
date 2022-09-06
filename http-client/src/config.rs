c2d666d79c11c3324510e4ab0d494ce496333c1a
quambene <contact.quambene@gmail.com>
2022-09-06 20:34:05 +0200

    Implement post request

--- http-client/src/config.rs
+++ http-client/src/config.rs
@@ -1,11 +1,20 @@
+mod request_method;
+
+pub(crate) use self::request_method::RequestMethod;
 use serde::Deserialize;
 use serde_json::Value;
 
+#[derive(Deserialize)]
+pub struct Header {
+    pub key: String,
+    pub value: String,
+}
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Config {
     pub api_url: String,
     pub api_endpoint: String,
-    pub request_method: String,
+    pub headers: Option<Vec<Header>>,
+    pub request_method: RequestMethod,
     pub request_body: Option<Value>,
 }
