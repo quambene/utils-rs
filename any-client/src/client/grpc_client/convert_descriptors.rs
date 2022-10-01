27a10919e9e87f60ff58ef746ff342c99767fde8
quambene <contact.quambene@gmail.com>
2022-10-01 23:34:31 +0200

    Convert descriptors III

--- any-client/src/client/grpc_client/convert_descriptors.rs
+++ any-client/src/client/grpc_client/convert_descriptors.rs
@@ -1,7 +1,9 @@
 use prost_types::{
-    source_code_info::Location, uninterpreted_option::NamePart, DescriptorProto,
-    EnumDescriptorProto, FieldDescriptorProto, FileDescriptorProto, FileOptions, MessageOptions,
-    ServiceDescriptorProto, SourceCodeInfo, UninterpretedOption, FieldOptions,
+    enum_descriptor_proto::EnumReservedRange, source_code_info::Location,
+    uninterpreted_option::NamePart, DescriptorProto, EnumDescriptorProto, EnumOptions,
+    EnumValueDescriptorProto, EnumValueOptions, FieldDescriptorProto, FieldOptions,
+    FileDescriptorProto, FileOptions, MessageOptions, MethodDescriptorProto, MethodOptions,
+    ServiceDescriptorProto, ServiceOptions, SourceCodeInfo, UninterpretedOption,
 };
 
 pub fn convert_file_descriptor_proto(
@@ -21,7 +23,7 @@ pub fn convert_file_descriptor_proto(
         enum_type: file_descriptor_proto
             .enum_type
             .into_iter()
-            .map(convert_enum_descriptor)
+            .map(convert_enum_descriptor_proto)
             .collect(),
         service: file_descriptor_proto
             .service
@@ -35,7 +37,8 @@ pub fn convert_file_descriptor_proto(
             .collect(),
         options: file_descriptor_proto.options.0.map(convert_file_options),
         source_code_info: file_descriptor_proto
-            .source_code_info.0
+            .source_code_info
+            .0
             .map(convert_source_code_info),
         syntax: file_descriptor_proto.syntax,
     }
@@ -45,11 +48,11 @@ fn convert_descriptor_proto(
     descriptor_proto: protobuf::descriptor::DescriptorProto,
 ) -> DescriptorProto {
     DescriptorProto {
-        name: todo!(),
-        field: todo!(),
-        extension: todo!(),
-        nested_type: todo!(),
-        enum_type: todo!(),
+        name: descriptor_proto.name,
+        field: descriptor_proto.field.into_iter().map(convert_field_descriptor_proto).collect(),
+        extension: descriptor_proto.extension.into_iter().map(convert_field_descriptor_proto).collect(),
+        nested_type: descriptor_proto.nested_type.into_iter().map(convert_descriptor_proto).collect(),
+        enum_type: descriptor_proto.enum_type.into_iter().map(convert_enum_descriptor_proto).collect(),
         extension_range: todo!(),
         oneof_decl: todo!(),
         options: descriptor_proto
@@ -57,19 +60,74 @@ fn convert_descriptor_proto(
             .0
             .map(|el| convert_message_options(*el)),
         reserved_range: todo!(),
-        reserved_name: todo!(),
+        reserved_name: descriptor_proto.reserved_name,
     }
 }
 
-fn convert_enum_descriptor(
+fn convert_enum_descriptor_proto(
     enum_descriptor_proto: protobuf::descriptor::EnumDescriptorProto,
 ) -> EnumDescriptorProto {
     EnumDescriptorProto {
-        name: todo!(),
-        value: todo!(),
-        options: todo!(),
-        reserved_range: todo!(),
-        reserved_name: todo!(),
+        name: enum_descriptor_proto.name,
+        value: enum_descriptor_proto
+            .value
+            .into_iter()
+            .map(convert_enum_value_descriptor_proto)
+            .collect(),
+        options: enum_descriptor_proto.options.0.map(convert_enum_options),
+        reserved_range: enum_descriptor_proto
+            .reserved_range
+            .into_iter()
+            .map(convert_enum_reserved_range)
+            .collect(),
+        reserved_name: enum_descriptor_proto.reserved_name,
+    }
+}
+
+fn convert_enum_value_descriptor_proto(
+    enum_value_descriptor_proto: protobuf::descriptor::EnumValueDescriptorProto,
+) -> EnumValueDescriptorProto {
+    EnumValueDescriptorProto {
+        name: enum_value_descriptor_proto.name,
+        number: enum_value_descriptor_proto.number,
+        options: enum_value_descriptor_proto
+            .options
+            .0
+            .map(convert_enum_value_options),
+    }
+}
+
+fn convert_enum_value_options(
+    enum_value_options: Box<protobuf::descriptor::EnumValueOptions>,
+) -> EnumValueOptions {
+    EnumValueOptions {
+        deprecated: enum_value_options.deprecated,
+        uninterpreted_option: enum_value_options
+            .uninterpreted_option
+            .into_iter()
+            .map(convert_uninterpreted_option)
+            .collect(),
+    }
+}
+
+fn convert_enum_options(enum_options: Box<protobuf::descriptor::EnumOptions>) -> EnumOptions {
+    EnumOptions {
+        allow_alias: enum_options.allow_alias,
+        deprecated: enum_options.deprecated,
+        uninterpreted_option: enum_options
+            .uninterpreted_option
+            .into_iter()
+            .map(convert_uninterpreted_option)
+            .collect(),
+    }
+}
+
+fn convert_enum_reserved_range(
+    enum_reserved_range: protobuf::descriptor::enum_descriptor_proto::EnumReservedRange,
+) -> EnumReservedRange {
+    EnumReservedRange {
+        start: enum_reserved_range.start,
+        end: enum_reserved_range.end,
     }
 }
 
@@ -77,9 +135,16 @@ fn convert_service_descriptor_proto(
     service_descriptor_proto: protobuf::descriptor::ServiceDescriptorProto,
 ) -> ServiceDescriptorProto {
     ServiceDescriptorProto {
-        name: todo!(),
-        method: todo!(),
-        options: todo!(),
+        name: service_descriptor_proto.name,
+        method: service_descriptor_proto
+            .method
+            .into_iter()
+            .map(convert_method_descriptor_proto)
+            .collect(),
+        options: service_descriptor_proto
+            .options
+            .0
+            .map(convert_service_options),
     }
 }
 
@@ -164,7 +229,11 @@ fn convert_file_options(file_options: Box<protobuf::descriptor::FileOptions>) ->
         php_namespace: file_options.php_namespace,
         php_metadata_namespace: file_options.php_metadata_namespace,
         ruby_package: file_options.ruby_package,
-        uninterpreted_option: file_options.uninterpreted_option.into_iter().map(convert_uninterpreted_option).collect(),
+        uninterpreted_option: file_options
+            .uninterpreted_option
+            .into_iter()
+            .map(convert_uninterpreted_option)
+            .collect(),
     }
 }
 
@@ -198,6 +267,53 @@ fn convert_field_options(field_options: Box<protobuf::descriptor::FieldOptions>)
         lazy: field_options.lazy,
         deprecated: field_options.deprecated,
         weak: field_options.weak,
-        uninterpreted_option: field_options.uninterpreted_option.into_iter().map(convert_uninterpreted_option).collect(),
+        uninterpreted_option: field_options
+            .uninterpreted_option
+            .into_iter()
+            .map(convert_uninterpreted_option)
+            .collect(),
+    }
+}
+
+fn convert_service_options(
+    service_options: Box<protobuf::descriptor::ServiceOptions>,
+) -> ServiceOptions {
+    ServiceOptions {
+        deprecated: service_options.deprecated,
+        uninterpreted_option: service_options
+            .uninterpreted_option
+            .into_iter()
+            .map(convert_uninterpreted_option)
+            .collect(),
+    }
+}
+
+fn convert_method_descriptor_proto(
+    method_descriptor_proto: protobuf::descriptor::MethodDescriptorProto,
+) -> MethodDescriptorProto {
+    MethodDescriptorProto {
+        name: method_descriptor_proto.name,
+        input_type: method_descriptor_proto.input_type,
+        output_type: method_descriptor_proto.output_type,
+        options: method_descriptor_proto
+            .options
+            .0
+            .map(convert_method_options),
+        client_streaming: method_descriptor_proto.client_streaming,
+        server_streaming: method_descriptor_proto.server_streaming,
+    }
+}
+
+fn convert_method_options(
+    method_options: Box<protobuf::descriptor::MethodOptions>,
+) -> MethodOptions {
+    MethodOptions {
+        deprecated: method_options.deprecated,
+        idempotency_level: MethodOptions::default().idempotency_level,
+        uninterpreted_option: method_options
+            .uninterpreted_option
+            .into_iter()
+            .map(convert_uninterpreted_option)
+            .collect(),
     }
 }
